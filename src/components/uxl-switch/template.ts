478831c827d43f8ffb129d5cfcd91fd1a891a8d7
Feliu Viaplana <info@altersites.com>
2019-07-12 12:58:21 +0200

    fix: styles & layout

--- src/components/uxl-switch/template.ts
+++ src/components/uxl-switch/template.ts
@@ -2,7 +2,7 @@ import { html } from "lit-element";
 import { UxlSwitch } from "./uxl-switch";
 export const template = (props: UxlSwitch) => html`
   <div class="wrapper ${props.enabled ? "enabled" : "disabled"}" part="wrapper">
-    <div class="option option__inactive ${props.active ? "" : "selected"}"" part="option-inactive">
+    <div class="option option__inactive ${props.active ? "" : "selected"}" part="option-inactive">
       ${props.inactiveName}
     </div>
     <div class="option option__active ${props.active ? "selected" : ""}" part="option-active">
